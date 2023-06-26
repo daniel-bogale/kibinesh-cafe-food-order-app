6bb2f49d8ace40058d5c58f3b821819c20f02e4e
danibog <dannybg090909@gmail.com>
2023-06-27 01:36:22 +0200

    meals summary edited

--- src/component/Meals/MealsSummary.js
+++ src/component/Meals/MealsSummary.js
@@ -3,10 +3,9 @@ import styles from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={styles.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Kibinesh Foods, Order From Anywhere</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your favorite meal from our broad selection of available meals.
       </p>
     </section>
   );
