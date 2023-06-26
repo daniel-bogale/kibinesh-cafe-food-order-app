789a1403dcc0f02c9e416fbfa073bafed3669258
danibog <dannybg090909@gmail.com>
2023-06-27 01:32:16 +0200

    summery text edited

--- src/component/Meals/MealsSummary.js
+++ src/component/Meals/MealsSummary.js
@@ -8,10 +8,6 @@ const MealsSummary = () => {
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
     </section>
   );
 };
